7efb455912d313b8fce4e76ea2e250892e69a457
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-03-27 13:05:12 +0100

    #7 Styling voor tablet toegevoegd en bijna af. Begin gemaakt aan desktop layout #13 NAV wordt juist getoond met breedte scherm.
    Animatie werkt nog niet foutloos van nav. Styling van page nog niet af

--- public/main.js
+++ public/main.js
@@ -2,17 +2,9 @@ function toggleMenu() {
     const menu = document.getElementById('nav-links');
     const content = document.getElementById('content');
 
-    if (menu.classList.contains('active')) {
-        menu.style.maxHeight = "0px";
-        setTimeout(() => {
-            menu.classList.remove('active');
-        }, 500); // Zorgt ervoor dat de class pas na de animatie wordt verwijderd
-    } else {
-        menu.classList.add('active');
-        setTimeout(() => {
-            menu.style.maxHeight = "300px"; // Pas aan afhankelijk van de menu-grootte
-        }, 10);
-    }
-
+    // Toggle de actieve status van het menu
+    menu.classList.toggle('active');
+    
+    // Toggle de verschuiving van de content
     content.classList.toggle('shifted');
 }
